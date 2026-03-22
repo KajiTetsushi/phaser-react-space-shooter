fb8bf3db0e6e1297890230ff54407a984b4b49dc
Kaji Tetsushi <kajitetsushi@outlook.com>
2026-03-22 23:54:38 +0800

    add enemy type 1

--- src/game/config.ts
+++ src/game/config.ts
@@ -5,3 +5,11 @@ export const PLAYER_CONFIG = {
         DRAG: 0.01,
     },
 };
+
+export const ENEMY_CONFIG = {
+    SCOUT: {
+        VERTICAL_VELOCITY: 20,
+        VERTICAL_VELOCITY_MAX: 200,
+        DRAG: 0.01,
+    },
+};
