754be862955a5c33f5d1c05b5d3ef8fe0e1c6dee
Kaji Tetsushi <kajitetsushi@outlook.com>
2026-03-24 00:38:18 +0800

    rename bullet to projectile

--- src/game/components/weapon/WeaponComponent.ts
+++ src/game/components/weapon/WeaponComponent.ts
@@ -4,12 +4,12 @@ import type InputComponent from '../input/InputComponent';
 type WeaponConfig = {
     projectileSpeed: number;
     /**
-     * In milliseconds. The minimum time between firing two consecutive bullets. This is used to control the firing rate of the weapon, preventing it from firing too rapidly and overwhelming the game with too many bullets at once.
+     * In milliseconds. The minimum time between firing two consecutive projectiles. This is used to control the firing rate of the weapon, preventing it from firing too rapidly and overwhelming the game with too many projectiles at once.
      */
     weaponCooldown: number;
     /**
-     * In seconds. The time it takes for a bullet to disappear after being fired.
-     * This is used to determine how long a bullet should remain active before being despawned and returned to the pool for reuse.
+     * In seconds. The time it takes for a projectile to disappear after being fired.
+     * This is used to determine how long a projectile should remain active before being despawned and returned to the pool for reuse.
      */
     projectileLifespan: number;
     projectilePoolSize: number;
@@ -22,25 +22,25 @@ export default class WeaponComponent {
     #inputComponent: InputComponent;
     #weaponConfig: WeaponConfig;
     /**
-     * Group to manage bullets fired by this weapon. It serves as a pool of bullet sprites that can be reused to optimize performance.
+     * Group to manage projectiles fired by this weapon. It serves as a pool of projectile sprites that can be reused to optimize performance.
      */
-    #bulletGroup: Physics.Arcade.Group;
+    #projectileGroup: Physics.Arcade.Group;
     /**
-     * Countdown timer for firing bullets. When it reaches 0, a bullet can be fired and the timer is reset to the interval value.
+     * Countdown timer for firing projectiles. When it reaches 0, a projectile can be fired and the timer is reset to the interval value.
      */
-    #fireBulletInterval: number = 0;
+    #fireProjectileInterval: number = 0;
 
-    constructor(gameObject: GameObjects.Container, inputComponent: InputComponent, bulletConfig: WeaponConfig) {
+    constructor(gameObject: GameObjects.Container, inputComponent: InputComponent, projectileConfig: WeaponConfig) {
         this.#gameObject = gameObject;
         this.#inputComponent = inputComponent;
-        this.#weaponConfig = bulletConfig;
+        this.#weaponConfig = projectileConfig;
 
-        this.#bulletGroup = this.#gameObject.scene.physics.add.group({
-            name: `bullets-${PhaserMath.RND.uuid()}`,
+        this.#projectileGroup = this.#gameObject.scene.physics.add.group({
+            name: `projectiles-${PhaserMath.RND.uuid()}`,
             enable: false,
         });
-        this.#bulletGroup.createMultiple({
-            key: 'bullet',
+        this.#projectileGroup.createMultiple({
+            key: 'projectile',
             quantity: this.#weaponConfig.projectilePoolSize,
             active: false,
             visible: false,
@@ -61,13 +61,13 @@ export default class WeaponComponent {
      * @param delta Timestep, in milliseconds, tied to the browser `requestAnimationFrame` callback, or roughly 60 times per second.
      */
     update(delta: number) {
-        this.#fireBullet(delta);
+        this.#fireProjectile(delta);
     }
 
-    #fireBullet(delta: number) {
-        this.#fireBulletInterval -= delta;
+    #fireProjectile(delta: number) {
+        this.#fireProjectileInterval -= delta;
 
-        if (this.#fireBulletInterval > 0) {
+        if (this.#fireProjectileInterval > 0) {
             return;
         }
 
@@ -75,53 +75,53 @@ export default class WeaponComponent {
             return;
         }
 
-        // Get the first inactive bullet from the pool and fire it.
-        const bullet: Physics.Arcade.Sprite | undefined = this.#bulletGroup.getFirstDead(false);
+        // Get the first inactive projectile from the pool and fire it.
+        const projectile: Physics.Arcade.Sprite | undefined = this.#projectileGroup.getFirstDead(false);
 
-        if (bullet == null) {
+        if (projectile == null) {
             return;
         }
 
         const x = this.#gameObject.x;
         const y = this.#gameObject.y + this.#weaponConfig.trajectoryYOffset;
-        bullet.enableBody(true, x, y, true, true);
-        if (bullet.body instanceof Physics.Arcade.Body) {
-            bullet.body.velocity.y -= this.#weaponConfig.projectileSpeed;
-            bullet.body.setSize(14, 18);
+        projectile.enableBody(true, x, y, true, true);
+        if (projectile.body instanceof Physics.Arcade.Body) {
+            projectile.body.velocity.y -= this.#weaponConfig.projectileSpeed;
+            projectile.body.setSize(14, 18);
         }
-        bullet.setState(this.#weaponConfig.projectileLifespan);
-        bullet.play('bullet');
-        bullet.setScale(0.8);
-        bullet.setFlipY(this.#weaponConfig.trajectoryFlipY);
+        projectile.setState(this.#weaponConfig.projectileLifespan);
+        projectile.play('projectile');
+        projectile.setScale(0.8);
+        projectile.setFlipY(this.#weaponConfig.trajectoryFlipY);
 
-        this.#fireBulletInterval = this.#weaponConfig.weaponCooldown;
+        this.#fireProjectileInterval = this.#weaponConfig.weaponCooldown;
     }
 
     /**
-     * Called on each physics world step to update the lifespan of each bullet.
+     * Called on each physics world step to update the lifespan of each projectile.
      * @param delta Timestep, in seconds, since the last world step, fixed to exactly 60Hz a.k.a. 1/60th of a second.
      */
     worldStep(delta: number) {
-        const bullets = this.#bulletGroup.getChildren() as Physics.Arcade.Sprite[];
-        bullets.forEach(this.#decayBulletLifespan(delta));
+        const projectiles = this.#projectileGroup.getChildren() as Physics.Arcade.Sprite[];
+        projectiles.forEach(this.#decayProjectileLifespan(delta));
     }
 
-    #decayBulletLifespan(delta: number) {
-        return (bullet: Physics.Arcade.Sprite) => {
-            if (!bullet.active) {
+    #decayProjectileLifespan(delta: number) {
+        return (projectile: Physics.Arcade.Sprite) => {
+            if (!projectile.active) {
                 return;
             }
 
-            if (typeof bullet.state !== 'number') {
+            if (typeof projectile.state !== 'number') {
                 return;
             }
 
-            // Decrease the bullet's remaining lifespan.
-            // If it reaches 0, despawn the bullet.
-            // The bullet will be put back to the pool and can be reused by future shots.
-            bullet.state -= delta;
-            if (bullet.state <= 0) {
-                bullet.disableBody(true, true);
+            // Decrease the projectile's remaining lifespan.
+            // If it reaches 0, despawn the projectile.
+            // The projectile will be put back to the pool and can be reused by future shots.
+            projectile.state -= delta;
+            if (projectile.state <= 0) {
+                projectile.disableBody(true, true);
             }
         };
     }
