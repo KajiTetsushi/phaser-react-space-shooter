cc89ad6fb5fc736cb3aeadecf66247c3a5dc08b8
Kaji Tetsushi <kajitetsushi@outlook.com>
2026-04-29 23:56:34 +0800

    add alt keys for movement in keyboard input

--- src/game/components/input/KeyboardInputComponent.ts
+++ src/game/components/input/KeyboardInputComponent.ts
@@ -1,14 +1,25 @@
-import type { Scene, Types } from 'phaser';
+import { Input, type Scene, type Types } from 'phaser';
 
 import InputComponent from './InputComponent';
 
 export default class KeyboardInputComponent extends InputComponent {
-    #cursorKeys!: Types.Input.Keyboard.CursorKeys;
+    #cursorKeys!: Types.Input.Keyboard.CursorKeys & {
+        w: Input.Keyboard.Key; // alt up
+        s: Input.Keyboard.Key; // alt down
+        a: Input.Keyboard.Key; // alt left
+        d: Input.Keyboard.Key; // alt right
+    };
     #inputLocked: boolean = false;
 
     constructor(scene: Scene) {
         super();
-        this.#cursorKeys = scene.input.keyboard!.createCursorKeys();
+        this.#cursorKeys = {
+            ...scene.input.keyboard!.createCursorKeys(),
+            w: scene.input.keyboard!.addKey(Input.Keyboard.KeyCodes.W),
+            s: scene.input.keyboard!.addKey(Input.Keyboard.KeyCodes.S),
+            a: scene.input.keyboard!.addKey(Input.Keyboard.KeyCodes.A),
+            d: scene.input.keyboard!.addKey(Input.Keyboard.KeyCodes.D),
+        };
     }
 
     setInputLocked(value: boolean) {
@@ -21,10 +32,10 @@ export default class KeyboardInputComponent extends InputComponent {
             return;
         }
 
-        this.up = this.#cursorKeys.up.isDown;
-        this.down = this.#cursorKeys.down.isDown;
-        this.left = this.#cursorKeys.left.isDown;
-        this.right = this.#cursorKeys.right.isDown;
+        this.up = this.#cursorKeys.up.isDown || this.#cursorKeys.w.isDown;
+        this.down = this.#cursorKeys.down.isDown || this.#cursorKeys.s.isDown;
+        this.left = this.#cursorKeys.left.isDown || this.#cursorKeys.a.isDown;
+        this.right = this.#cursorKeys.right.isDown || this.#cursorKeys.d.isDown;
         this.shoot = this.#cursorKeys.space.isDown;
     }
 }
