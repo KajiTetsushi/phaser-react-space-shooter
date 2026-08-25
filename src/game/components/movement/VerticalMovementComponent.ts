c8f61eb6499b06f551349130c1699c492bff9812
Kaji Tetsushi <kajitetsushi@outlook.com>
2026-08-25 16:55:31 +0800

    consolidate types into shared file

--- src/game/components/movement/VerticalMovementComponent.ts
+++ src/game/components/movement/VerticalMovementComponent.ts
@@ -1,30 +1,7 @@
 import type { Physics } from 'phaser';
 
 import type InputComponent from '../input/InputComponent';
-
-interface MovementComponentConfigInertialess {
-    /**
-     * Fixed value of how fast the object will move in the given direction.
-     */
-    velocity: number;
-}
-
-interface MovementComponentConfigPhysical {
-    /**
-     * A value to increase, per frame, of how fast the object will move in the given direction.
-     */
-    velocityIncrement?: number;
-    /**
-     * An upper limit value to how fast the object can increase to in the given direction.
-     */
-    velocityMaximum?: number;
-    /**
-     * A value to set the amount of directional slow down in pixels per second when the directional control is released.
-     */
-    drag?: number;
-}
-
-type MovementComponentConfig = MovementComponentConfigInertialess | MovementComponentConfigPhysical;
+import type { MovementComponentConfig } from './movement.types';
 
 const DIRECTION = {
     UP: -1,
